e470136f151158a1c296abc931520588b1b00be8
Ignacio Moral <a01028470@itesm.mx>
2020-09-14 20:55:25 -0500

    Add Calcpy and textoTarea

---
+++ calcpy.py
@@ -0,0 +1,12 @@
+a=int(input("Escribe el primer numero:\n"))
+b=int(input("Escribe el segundo numero:\n"))
+
+suma=a+b
+resta=a-b
+mult=a*b
+div=a/b
+
+print("La suma es ", suma)
+print("La resta es ", resta)
+print("La multiplicacion es ", mult)
+print("La division es ", div)
